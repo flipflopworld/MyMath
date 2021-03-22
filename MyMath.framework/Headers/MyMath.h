012b8998bf97705e1f35938091681aebb74e1b33
Narendar <narendar.saini@xymob.com>
2021-03-22 16:45:30 +0530

    Added framework files

---
+++ MyMath.framework/Headers/MyMath.h
@@ -0,0 +1,18 @@
+//
+//  MyMath.h
+//  MyMath
+//
+//  Created by apple on 04/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyMath.
+FOUNDATION_EXPORT double MyMathVersionNumber;
+
+//! Project version string for MyMath.
+FOUNDATION_EXPORT const unsigned char MyMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyMath/PublicHeader.h>
+
+
